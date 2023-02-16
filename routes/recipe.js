3392709d18087ebe17958676eb65b6fb2031dd2f
Joselyn <cuzco.joselyn@gmail.com>
2023-02-15 23:54:16 -0500

    added route

--- routes/recipe.js
+++ routes/recipe.js
@@ -6,31 +6,54 @@ export default function recipeRouter() {
 
   router.get("/", async (request, response) => {
     const allRecipes = await prisma.recipe.findMany({
-        where: {
-            userId: 1,
-        },
-        include: {
-            user: true,
-        }
+      where: {
+        userId: 1,
+      },
+      include: {
+        user: true,
+      },
     });
 
-        response.status(200).json({
-            sucess: true,
-            recipe: allRecipes,
-        });
+    response.status(200).json({
+      sucess: true,
+      recipe: allRecipes,
+    });
+  });
+
+  router.get("/:userId/:recipeId", async function (request, response) {
+    try {
+      const getRecipe = await prisma.recipe.findMany({
+        where: {
+          id: parseInt(request.params.recipeId),
+          user: {
+            id:{
+                equals:parseInt(request.params.userId)
+            }
+          }
+        }
+      });
+
+      response.status(200).json({
+        sucess: true,
+        data: getRecipe,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   });
 
   router.post("/", async (request, response) => {
     const newRecipe = await prisma.recipe.create({
-        data: {
-            name: request.doby.recipe,
-            useId: 1,
-        },
+      data: {
+        name: request.body.recipe,
+        userId: 1,
+        description: request.body.description
+      },
     });
     console.log(newRecipe);
 
     response.status(201).json({
-        sucess: true,
+      sucess: true,
     });
   });
 
